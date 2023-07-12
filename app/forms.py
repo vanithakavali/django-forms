e74086a7a18ea7411e25c2684f58c0669dbe5949
vanithakavali <kavalivanitheswari@gmail.com>
2023-07-12 14:18:11 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 
 from django import forms
-
+from django.core import validators
 def validate_for_a(svalue):
     if svalue[0].lower()=='a':
         raise forms.ValidationError('The first letter not to be correct')
@@ -13,10 +13,12 @@ def validate_for_len(name):
 class StudentForm(forms.Form):
     email=forms.EmailField()
     remail=forms.EmailField()
-    sname=forms.CharField(max_length=100,validators=[validate_for_a,validate_for_len])
+    sname=forms.CharField(max_length=100,validators=[validate_for_a,validators.RegexValidator])
     sage=forms.IntegerField()
     email=forms.EmailField()
+    url=forms.URLField()
     botcatcher=forms.CharField(widget=forms.HiddenInput,required=False)
+    moblie=forms.CharField(max_length=10,min_length=10,validators=[validators.RegexValidator('[6-9]\d{9}')])
     def clean(self):
         e=self.cleaned_data['email']
         re=self.cleaned_data['remail']
